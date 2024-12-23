1a2a1eb04d71e4e0395734bcacb68beecc1d01d0
Nick Kuhle <nickkuhle@yahoo.com>
2024-12-23 11:13:05 +0000

    another button attempt

--- salsa/app/new-post.tsx
+++ salsa/app/new-post.tsx
@@ -37,7 +37,7 @@ export default function NewPost({ user }: { user: User }) {
           placeholder="Was it salty or what?"
         />
       </div>
-      <input type="submit" className="bg-inherit border border-gray-800 rounded-lg"/>
+      <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
     </form>
   );
 }
